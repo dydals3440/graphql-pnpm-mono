6e3a5ac2b1d2f92f894e4bd678b6ecb20a2e138e
김용민 <dydals3440@gmail.com>
2024-12-15 00:43:06 +0900

    [17-7] graphql 데이터 처리 추가

--- packages/music-server/src/resolvers.ts
+++ packages/music-server/src/resolvers.ts
@@ -9,6 +9,26 @@ export const resolvers = {
       prisma.artist.findMany({
         include: { albums: true },
       }),
+    artist: async (_: any, { id }: { id: string }) => {
+      const artist = await prisma.artist.findUnique({
+        where: { id: parseInt(id) },
+        include: { albums: true },
+      });
+      return artist;
+    },
+    songs: async () =>
+      prisma.song.findMany({
+        include: {
+          genres: true,
+          album: {
+            include: { artist: true },
+          },
+        },
+      }),
+    albums: async () =>
+      prisma.album.findMany({
+        include: { artist: true, songs: true },
+      }),
   },
   Mutation: {
     // 첫번째 파라미터는 root에 관한 부분
@@ -21,6 +41,63 @@ export const resolvers = {
         },
       });
       return genre;
+    },
+    addArtist: async (_: any, { name }: { name: string }) => {
+      const artist = await prisma.artist.create({
+        data: { name },
+      });
+      return artist;
+    },
+    addSong: async (
+      _: any,
+      {
+        title,
+        albumId,
+        genreIds,
+      }: { title: string; albumId: string; genreIds: string[] }
+    ) => {
+      const song = await prisma.song.create({
+        data: {
+          title,
+          album: {
+            connect: { id: parseInt(albumId) },
+          },
+          genres: {
+            connect: genreIds.map((id) => ({ id: parseInt(id) })),
+          },
+        },
+        include: { genres: true, album: true },
+      });
+      return song;
+    },
+    addAlbum: async (
+      _: any,
+      {
+        title,
+        artistId,
+        releaseDate,
+      }: { title: string; artistId: string; releaseDate: string }
+    ) => {
+      const artist = await prisma.artist.findUnique({
+        where: { id: parseInt(artistId) },
+      });
+
+      if (!artist) {
+        throw new Error(`Artist with id ${artistId} not found`);
+      }
+
+      const album = await prisma.album.create({
+        data: {
+          title,
+          artist: {
+            connect: { id: parseInt(artistId) },
+          },
+          releaseDate: new Date(releaseDate),
+        },
+        // create 할 떄 artist에 대한 정보를, 조회함.
+        include: { artist: true },
+      });
+      return album;
     },
   },
 };
--- packages/music-server/src/typeDefs.ts
+++ packages/music-server/src/typeDefs.ts
@@ -8,7 +8,7 @@ export const typeDefs = gql`
   }
   type Album {
     id: ID!
-    name: String!
+    title: String!
     artist: Artist!
     song: [Song!]
   }
@@ -32,10 +32,15 @@ export const typeDefs = gql`
     # 특정 아티스트에 대해서, 갖고오는 케이스가 있을 것 같아, 아티스트쿼리 작성
     # id를 받아서, Artist를 리턴
     artist(id: ID!): Artist
+    # 노래에 대한 리스트를 가져오도록 함.
+    songs: [Song!]!
+    albums: [Album!]!
   }
   # 서버에 데이터베이스를 변경해야 하는 부분을 Mutation으로 정의
   type Mutation {
     addGenre(name: String!): Genre!
     addArtist(name: String!): Artist!
+    addSong(title: String!, albumId: ID!, genreIds: [ID!]!): Song!
+    addAlbum(title: String!, artistId: ID!, releaseDate: String!): Album!
   }
 `;
